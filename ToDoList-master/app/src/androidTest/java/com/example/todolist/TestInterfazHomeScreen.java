1ec150ad723812d0937cbf895141907bcd207c52
DavidCC <96200005+DavidCCGithubURJC@users.noreply.github.com>
2024-05-03 11:49:22 +0200

    Tests Interfaz
    Añadidos test de interfaz para testear los botones de Start, Cancelar (al crear una tarea) y el botón para ir a la pantalla de crear una tarea.

---
+++ ToDoList-master/app/src/androidTest/java/com/example/todolist/TestInterfazHomeScreen.java
@@ -0,0 +1,30 @@
+package com.example.todolist;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestInterfazHomeScreen {
+    @Rule
+    public ActivityScenarioRule<HomeScreen> activityRule = new ActivityScenarioRule<>(HomeScreen.class);
+    @Test
+    public void testFirstFragmentScreen() {
+        onView(withId(R.id.firstFragment)).perform(click());
+        onView(withId(R.id.textousuario)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testButtonCancel() {
+        onView(withId(R.id.firstFragment)).perform(click());
+        onView(withId(R.id.cancelar)).perform(click());
+        onView(withId(R.id.listView)).check(matches(isDisplayed()));
+    }
+
+}
---
+++ ToDoList-master/app/src/androidTest/java/com/example/todolist/TestInterfazMainActivity.java
@@ -0,0 +1,23 @@
+package com.example.todolist;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestInterfazMainActivity {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void testStartButton() {
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withId(R.id.frame_container)).check(matches(isDisplayed()));
+    }
+
+}
